d7ba9d1afec6ebaf32cb7538c562c61da264e93a
abbylute <aby.lute@gmail.com>
2023-01-20 00:31:34 +0000

    initial commit

---
+++ file_control_template.py
@@ -0,0 +1,14 @@
+# Template for a file_control.py file that can be imported to get file and directory locations
+
+# List of file and directory locations
+
+# path to directory containing the gmst tables
+gmst_table_dir = ''
+# path to directory containing the model tables
+model_table_dir = ''
+# path to directory containing the warming year tables
+warming_year_table_dir = ''
+# path to directory containing the aggregated warming year data
+warming_year_aggregated_data_dir = ''
+# path to directory containing all data files
+data_dir = ''
